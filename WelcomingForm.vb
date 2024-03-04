8706dcdcca2e541f4da46288250eef03c14981a1
Antony Omondi <124069102+antonio-chief@users.noreply.github.com>
2024-03-04 21:59:13 +0300

    Update WelcomingForm.vb

--- WelcomingForm.vb
+++ WelcomingForm.vb
@@ -1,13 +1,16 @@
 ﻿Public Class WelcomingForm
+    'Exit button that closes the application
     Private Sub ExitButton_Click(Sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
 
+    'Load button that opens the "Object Demonstration Form"
     Private Sub LoadObjectButton_Click(Sender As Object, e As EventArgs) Handles LoadObjectButton.Click
         Dim ObjectDemoForm As New ObjectDemoForm()
         ObjectDemoForm.Show()
     End Sub
 
+    'To maximise the application window and change its colour to red
     Private Sub WelcomingForm_Click(Sender As Object, e As EventArgs) Handles MyBase.Click
         WindowState = FormWindowState.Maximized
         BackColor = Color.Red
